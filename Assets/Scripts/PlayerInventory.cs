bc707540510c768c6c55c9031ee4dcd0374bfacc
Leo-paul <leopaulmartin@hotmail.fr>
2020-11-14 23:26:27 +0100

    add inventory store and load trigger

---
+++ Assets/Scripts/PlayerInventory.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInventory : MonoBehaviour
+{
+    public InventoryObject inventoryObject;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var item = other.GetComponent<GroundItem>();
+        if (item)
+        {
+            inventoryObject.AddItem(new Item(item.item), 1);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        inventoryObject.Clear();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            inventoryObject.Save();
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            inventoryObject.Load();
+        }
+    }
+}
